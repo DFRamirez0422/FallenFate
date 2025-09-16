d96b248ac3ee1e10978cd9544742c883a3005876
Boobombdigity <erikaguiar2017@gmail.com>
2025-09-16 12:38:29 -0400

    Hitbox script push

---
+++ Assets/NPA_PlayerPrefab/Scripts/Hitbox.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private AttackData attackData; 
+    private GameObject owner;
+
+    public void Initialize(AttackData data, GameObject ownerObj)
+    {
+        attackData = data;
+        owner = ownerObj;
+
+
+        BoxCollider col = GetComponent<BoxCollider>();
+        if (col != null) col.size = attackData.hitboxSize;
+
+
+        Destroy(GameObject, attackData.attackDuration);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == owner) return; //dont hit self
+
+        Health health = other.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(attackData.damage);
+        }
+    }
+}
