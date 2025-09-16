7c7a01f1a6731c29a8db478582aca23fc1b4ca4e
Michael Hernandez <mikegtr_305@hotmail.com>
2025-09-16 12:43:39 -0400

    Started New Player Combat System
    Added multiple simple combat scripts to start fresh.

--- Assets/NPA_PlayerPrefab/Scripts/PlayerCombat.cs
+++ Assets/NPA_PlayerPrefab/Scripts/PlayerCombat.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
 
-public class PlayerCombat : MonoBehaviour
+namespace NPA_PlayerPrefab.Scripts
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerCombat : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private Transform attackSpawnPoint; // Where hitbox spawns
+        [SerializeField] private GameObject hitBoxPrefab;    // Hitbox prefab
+        [SerializeField] private AttackData defaultAttack;   // Attack stats
         
-    }
+        [Header("Input Settings")]
+        [SerializeField] private KeyCode attackKey = KeyCode.Mouse0; // Basic attack button
 
-    // Update is called once per frame
-    void Update()
-    {
+        private bool isAttacking = false;
         
+        // Update is called once per frame
+        void Update()
+        {
+            HandleAttackInput();
+        }
+
+        private void HandleAttackInput()
+        {
+           if (Input.GetButtonDown(attackKey) && !isAttacking)
+           {
+               Attack(defaultAttack);
+           }
+        }
+        
+        private void Attack(AttackData attackData)
+        {
+            if (attackData == null || hitBoxPrefab == null || attackSpawnPoint == null) return;
+            
+            isAttacking = true;
+
+            // Spawn hitbox
+            GameObject hb = Instantiate(hitBoxPrefab, attackSpawnPoint.position, attackSpawnPoint.rotation);
+            Hitbox hbComp = hb.GetComponent<Hitbox>();
+            if (hbComp != null)
+            {
+                hbComp.Initialize(attackData, this.gameObject);
+            }
+            
+            Invoke(nameof(ResetAttack), attackData.attackDuration);
+        }
+
+        private void ResetAttack()
+        {
+            isAttacking = false;
+        }
     }
 }
