eaed8670e5ad185e8fb75477adc041d411556176
NathanFWhite <Nathan.White003@mymdc.net>
2025-09-24 13:59:31 -0400

    Added ElenaAI

---
+++ Assets/AAA_FallenFate/Scripts/Ai Scripts/ElenaAI.cs
@@ -0,0 +1,89 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ElenaAI : MonoBehaviour
+{
+    
+    private Transform player;
+    private Transform self;
+    private NavMeshAgent agent;
+
+    public int followDistance;
+    public bool CombatToggle = false;
+    public GameObject[] objects;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        self = GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+    void Start()
+    {
+        objects = GameObject.FindGameObjectsWithTag("Cover");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (CombatToggle == false)
+        {
+            FollowPlayer();
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CombatToggle = true;
+            }
+        }
+
+        if (CombatToggle == true)
+        {
+            TakeCover();
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                CombatToggle = false;
+            }
+        }       
+    }
+
+    private void FollowPlayer()
+    {
+        float distanceToPlayer = Vector3.Distance(player.position, self.transform.position);
+
+        if (player != null)
+        {
+            if (distanceToPlayer > followDistance)
+            {
+                agent.SetDestination(player.position);
+            }
+            else
+            {
+                agent.SetDestination(self.position);
+                return;
+            }
+        }
+    }
+
+    private void TakeCover()
+    {
+        GameObject closest = null;
+        float minDistSq = float.MaxValue;
+        Vector3 tPos = self.position;
+
+        foreach (var obj in objects)
+        {
+            float distSq = (obj.transform.position - tPos).sqrMagnitude;
+            if (distSq < minDistSq)
+            {
+                minDistSq = distSq;
+                closest = obj;
+            }
+        }
+
+        agent.SetDestination(closest.transform.position);
+    }
+}
