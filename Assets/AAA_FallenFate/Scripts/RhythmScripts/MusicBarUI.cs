68a32bd824210b945f0026ed4401c4122304913c
JoseEscobedo02 <jose.escobedo002@mymdc.net>
2025-09-28 18:40:53 -0400

    Revamp of UI to be closer to new requirement.
    Revamped the UI to be closer in capability to Hi Fi Rush as per given instructions. A prefab is included that will be worked on for the final project.
    FIXME: There is a nasty non-deterministic bug occurring because of Unity start up lag. This means the music and UI may not sync up properly. Ongoing support for fix soon!

--- Assets/AAA_FallenFate/Scripts/RhythmScripts/MusicBarUI.cs
+++ Assets/AAA_FallenFate/Scripts/RhythmScripts/MusicBarUI.cs
@@ -8,6 +8,7 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace NPA_RhythmBonusPrefabs
@@ -33,119 +34,206 @@ namespace NPA_RhythmBonusPrefabs
 
         TODO: talk with the UI designers and play the Ratatan demo to find out how to implement this behavior.
         TODO: fix up the code proper instead of a shoddy copy of PulseToBeat.cs
+
+        UPDATE (as of September 27th, 2025):
+        The new game to use as a base reference is High-Fi Rush. After playing through the game for about 90 or so
+        minutes and collecting gameplay videos through my own inputs, I can safely say that the game
+        was well worth what I paid for as it sat in my steam library. I couldn't put it down, do you see? The
+        engaging gameplay paired with my xbox controller made it quite fun in a way that couldn't be
+        replicted by online videos. I defeated the big robot hands guy and decided to stop for now so I
+        can actually sleep hahaha. Anyways, we'll try to replicate that UI now in accordanace to the
+        Rhythm System Document document.
     */
     public class MusicBarUI : MonoBehaviour
     {
+        [Tooltip("The instance of the rhythm music player.")]
         [SerializeField] private RhythmMusicPlayer music;  // Music clock
 
-        [Tooltip("Game object of the music cursor in the music bar.")]
-        [SerializeField] private GameObject m_CursorObject;
-
-        [Tooltip("Game object of the music staff itself.")]
+        [Tooltip("Music staff boundary box.")]
         [SerializeField] private RectTransform m_StaffObject;
 
-        [Header("Pulses")]
+        [Tooltip("Prefab for the beat marker object.")]
+        [SerializeField] private GameObject m_BeatMarkerPrefab;
 
-        [Tooltip("How big pulse gets")]
-        [SerializeField] private float pulseSize = 1.15f;
-        
-        [Tooltip("How fast pulse returns")]
-        [SerializeField] private float returnSpeed = 5f;
-        
-        [Tooltip("1=Quarter, 1=Eighths, 4=Sixteenths, 0.5=Half Notes")]
-        [SerializeField] private float subdivision = 1f;   // Which note value to follow
-        
-        [Tooltip("Auto-pulse with beat?")]
-        [SerializeField] private bool autoPlay = true;
+        /*
+            How do we implement the moving beat makers?
+            Well, we keep a list of all the instances of the prefab that we initialize on Start(). This list
+            all start as inactive at first at both extremes of the staff. On each update, activate one of
+            the markers on both ends and move them closer to the center. On activation, they fade from
+            transparent to opaque over time. On each beat, we again cycle by activating more markers from
+            both ends. Once some markers reach the center, we deactivate and move them to the ends.
 
-        [Header("Calibration")]
-        [Tooltip("Extra offset (in seconds) to shift pulses later/earlier")]
-        [SerializeField] private float pulseOffsetSec = 0f;
-        
-        private Vector3 startPosition; // Original position of UI
-        private Vector3 startSize; // Original scale of UI
-        
+            I envision this working as an ad-hoc FIFO queue structure. The first to go in are the first to be
+            swapped out.
+
+            Now internally, the list is ordered such that either end of the staff correspond to either end
+            of the list. Thus retrieving by index is relatively simple. On the other hand, a variable
+            is needed to keep track on which position in the queue is the tail to send swapped out markers to.
+
+            To keep things simple, about four beats will be on the staff, so eight entries total.
+        */
+        private List<GameObject> m_BeatMarkersList = new List<GameObject>();
+
+        // Extreme positions of the music staff.
+        private Vector3 m_LeftmostEnd;
+        private Vector3 m_RightmostEnd;
+
+        // Amount of time since initialization or activation.
+        private float m_ElapsedTime = 0.0f;
+
+        // As mentioned above, in order to keep things simple, we will initialize to 4 beats on the staff.
+        private const int s_NumBeatMarkers = 4;
+
+        // Current index within the queue to the head.
+        private int m_CurrentHeadIndex = 0;
+
+        // Current index within the queue to the tail.
+        private int m_CurrentTailIndex = 0;
+
         private void Start()
         {
-            if (!m_CursorObject)
+            // As stated in the note above, we initialize all the needed markers right away and make them inactive
+            // immediately to only be made active when needed.
+            Vector3[] staff_corners = new Vector3[4];
+            m_StaffObject.GetLocalCorners(staff_corners);
+
+            m_LeftmostEnd = Vector3.Lerp(staff_corners[0], staff_corners[1], 0.5f);//Vector3.zero;
+            m_RightmostEnd = Vector3.Lerp(staff_corners[2], staff_corners[3], 0.5f);//new Vector3(m_StaffObject.rect.width, 0, 0);
+            var rotation = Quaternion.identity;
+
+            for (int i = 0; i < s_NumBeatMarkers; i++)
             {
-                throw new Exception("No music bar cursor objet was set. Aborting!");
+                AddNewMarker(m_LeftmostEnd);
             }
 
-            if (!music) music = FindAnyObjectByType<RhythmMusicPlayer>();
-            startSize = m_CursorObject.transform.localScale;
-            startPosition = m_CursorObject.transform.position;
+            for (int i = 0; i < s_NumBeatMarkers; i++)
+            {
+                AddNewMarker(m_RightmostEnd);
+            }
 
-            // Moves the cursor back just enough to adjust for any lag from the song.
-            // Probably should find a way to bring in the game's start time as well; a lot of
-            // the lag I am encountering stems from just Unity itself.
-            float lag_time = (float)(music.SongStartDSP % music.BeatSec);
-            m_CursorObject.transform.Translate(-lag_time, 0.0f, 0.0f);
-            
-            if (autoPlay && music != null) 
-                StartCoroutine(PulseRoutine());
+            // GIANT FIXME:
+            //
+            // There is a nasty non-determinstic bug occuring because of Unity start up lag.
+            // This makes the UI not match up properly to the music and not match up properly to the
+            // beat entering the center of the screen. This line is suppoosed to help fix it
+            // but it's only a bit of a band-aid. It doesn't always work either...
+            m_ElapsedTime += (float)music.SongStartDSP + (music.BeatSec * 0.25f);
         }
 
-        // Smoothly return scale back to start size
+        // Helper function for initializating new beat markers.
+        private void AddNewMarker(Vector3 position)
+        {
+            // All new beat markers start life deactivated and only activated upon request.
+            var new_marker = Instantiate(m_BeatMarkerPrefab);
+            var marker_script = new_marker.GetComponent<UIElementFader>();
+            marker_script.deactivate(0.0f);
+
+            // WHY IS UNITY SO STUPID WITH THIS? EVERY TIME WITH UI PREFABS.
+            // If I'm giving you a direct position, why place it at some random point on the screen?
+            // Then the X component is right while the Y is wrong. WHY????
+            //
+            // ...above, the reaction of spending about an hour on this issue. Writing down on paper the
+            // entire heirarchy isn't enough it seems.
+            //new_marker.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.5f, 0.5f);
+            new_marker.transform.SetParent(m_StaffObject, false);
+            new_marker.transform.localPosition = position;
+
+            m_BeatMarkersList.Add(new_marker);
+        }
+
         private void Update()
+        {
+            handleBeatAppearance();
+            updateMarkerMovement();
+            handleBeatOnCenter();
+            m_ElapsedTime += Time.deltaTime;
+        }
+
+        // Handles the general cycle for beats in terms of appearance, not related to their actual movement.
+        private void handleBeatAppearance()
+        {
+            if (m_ElapsedTime > music.BeatSec)
+            {
+                // For the first couple of beats, we have to activate them manually.
+                if (m_CurrentHeadIndex < s_NumBeatMarkers)
+                {
+                    var marker_left_script = getBeatMarkerOnLeft(m_CurrentHeadIndex).GetComponent<UIElementFader>();
+                    var marker_right_script = getBeatMarkerOnRight(m_CurrentHeadIndex).GetComponent<UIElementFader>();
+
+                    marker_left_script.activate(music.BeatSec);
+                    marker_right_script.activate(music.BeatSec);
+                }
+
+                m_CurrentHeadIndex++;
+                m_ElapsedTime = 0.0f;
+            }
+        }
+
+        // Handles the situation when the beat markers move to the center of the screen in order to reset
+        // them.
+        private void handleBeatOnCenter()
+        {
+            // Once the beat markers reach the center of the screen:
+            // - Deactivate them, effectively making them invisible to be moved and reset.
+            // - Move them to the extremes again.
+            // - Activate them to start the fade in process via its own script.
+            GameObject marker_left = getBeatMarkerOnLeft(0);
+            GameObject marker_right = getBeatMarkerOnRight(0);
+
+            // We can assume the beats are moving at exactly the same pace so no need to check the other end.
+            if (marker_left.transform.localPosition.x > 0.0f)
+            {
+                var marker_left_script = marker_left.GetComponent<UIElementFader>();
+                var marker_right_script = marker_right.GetComponent<UIElementFader>();
+
+                marker_left_script.deactivate(0.0f);
+                marker_right_script.deactivate(0.0f);
+
+                marker_left.transform.localPosition = m_LeftmostEnd;
+                marker_right.transform.localPosition = m_RightmostEnd;
+
+                marker_left_script.activate(music.BeatSec * 2.0f);
+                marker_right_script.activate(music.BeatSec * 2.0f);
+
+                m_CurrentTailIndex++;
+            }
+        }
+
+        // Main update for the beat markers handling transforms.
+        private void updateMarkerMovement()
         {
             // Implement the movement speed based on the beats per second and the staff dimensions.
             // If the cursor moves past the end, snap it back to the beginning.
             // Of course this carries the assumption that the cursor is on the correct position to
             // begin with, so not very ideal.
             int num_beats_per_measure = 4;
-            float staff_dimension = m_StaffObject.rect.width;
+            float staff_dimension = m_StaffObject.rect.width / 2.0f;
             float delta_speed = (staff_dimension / (float)num_beats_per_measure) / music.BeatSec * Time.deltaTime;
-            m_CursorObject.transform.Translate(delta_speed, 0.0f, 0.0f);
+            int num_beat_markers = Math.Min(s_NumBeatMarkers, m_CurrentHeadIndex);
 
-            if (m_CursorObject.transform.position.x > startPosition.x + staff_dimension)
+            for (int i = 0; i < num_beat_markers; i++)
             {
-                m_CursorObject.transform.position = startPosition;
+                getBeatMarkerOnLeft(i).transform.Translate(delta_speed, 0.0f, 0.0f);
             }
 
-            m_CursorObject.transform.localScale = Vector3.Lerp(m_CursorObject.transform.localScale, startSize, Time.deltaTime * returnSpeed);
-        }
-
-        // Trigger a visual pulse
-        public void Pulse()
-        {
-            m_CursorObject.transform.localScale = startSize * pulseSize;
-        }
-
-        // Calculates the beats per second given a dynamically-changing tempo.
-        private double CalcBeatPerSec()
-        {
-            // Jose E.:
-            // Recalculate if the tempo has been changed.
-            // Clamping is needed to avoid locking Unity.
-            // Removing the clamping will lock Unity entirely and require force close. Please do not remove.
-            // Please do not ask what the magic numbers mean either. They're supposed to be sensible limits.
-            double beatSec = 60 / (music.BPM * subdivision);
-            return System.Math.Clamp(beatSec, 1.0 / 10.0, 2.0);
-        }
-
-        // Pulses every quarter note using the song's BPM
-        IEnumerator PulseRoutine()
-        {
-            if (music == null || music.BPM <= 0) yield break;
-            
-            double beatSec = CalcBeatPerSec();
-            double nextBeat = music.SongStartDSP + pulseOffsetSec + beatSec;
-            
-            while (AudioSettings.dspTime < nextBeat)
-                yield return null;
-
-            while (true)
+            for (int i = 0; i < num_beat_markers; i++)
             {
-                Pulse();
-                nextBeat += beatSec;
-
-                // Wait until DSP time reaches next beat
-                while (AudioSettings.dspTime < nextBeat)
-                    yield return null;
-                    
-                beatSec = CalcBeatPerSec();
+                getBeatMarkerOnRight(i).transform.Translate(-delta_speed, 0.0f, 0.0f);
             }
+        }
+
+        // Returns the beat marker to the left of the center by its index. The lower the number, the closer to
+        // the center.
+        private GameObject getBeatMarkerOnLeft(int index)
+        {
+            return m_BeatMarkersList[(index + m_CurrentTailIndex) % s_NumBeatMarkers];
+        }
+
+        // Returns the beat marker to the right of the center by its index. The lower the number, the closer to
+        // the center.
+        private GameObject getBeatMarkerOnRight(int index)
+        {
+            return m_BeatMarkersList[m_BeatMarkersList.Count - 1 - (index + m_CurrentTailIndex) % s_NumBeatMarkers];
         }
     }
 }
