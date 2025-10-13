27a79c76a08f79cd33d846c7a242bf4b2f76efb4
Rodney Torres <altodor.tv@gmail.com>
2025-10-13 12:33:38 -0400

    Wrote an outline for boss AI logic
    Im just commiting this to checkpoint my progress but I edited the BossAI script with comments

--- Assets/AAA_FallenFate/Scenes/RT_Assets/Scripts/RT_BossAI.cs
+++ Assets/AAA_FallenFate/Scenes/RT_Assets/Scripts/RT_BossAI.cs
@@ -7,11 +7,13 @@ public class RT_BossAI : MonoBehaviour
 
     // Phase 1 Logic
     // Play the boss song
+    // Set boss HP to X (placeholder HP)
     // Start attack pattern 1
 
     //Attack pattern 1 logic (4 attacks)
 
     // Add boss invunerability
+    // Play Idle Animation
     // Do FlySummonAttack
     // Play Idle animation
     // Do MachineGunRiff (This should line up to the song)
@@ -22,8 +24,9 @@ public class RT_BossAI : MonoBehaviour
     // Play Idle animation
 
     // After attack pattern 1 remove invunerability and start using CloseRangeAttacks (1 out of 3 potential attacks)
-    // If boss health reaches 0 trigger a QTE event
-    // 4 QTE's happen and the player needs to hit them to go into phase 2
+    // In between the CloseRangeAttacks play the Idle animation
+    // If boss health reaches 0 stagger the boss and allow the boss to be hit to trigger a QTE event
+    // 4 QTE's happen and the player needs to hit them correctly to go into phase 2
     // if they fail to hit the QTE's reset the players position to the entrance, reset boss health, and start phase 1
     // if they succeed the QTE's reset the players position to the entrance and reset boss health. Start phase 2
 
@@ -31,11 +34,13 @@ public class RT_BossAI : MonoBehaviour
 
     // Phase 2 logic
     // Play the boss song
+    // Set boss HP to X (placeholder HP)
     // Start attack pattern 2
 
     // Attack pattern 2 logic (4 attacks)
 
     // Add boss invunerability
+    // Play Idle animation
     // Do BeetleSummonAttack
     // Play Idle animation
     // Do MachineGunRiff (This should line up to the song)
@@ -46,7 +51,8 @@ public class RT_BossAI : MonoBehaviour
     // Play Idle animation
 
     // After attack pattern 2 remove invunerability and start using CloseRangeAttacks (1 out of 3 potential attacks)
-    // If boss health reaches 0 trigger a QTE event
+    // In between the CloseRangeAttacks play the Idle animation
+    // If boss health reaches 0 stagger the boss and allow the boss to be hit to trigger a QTE event
     // 7 QTE's happen and the player needs to hit them to go into phase 3
     // if they fail to hit the QTE's reset the players position to the entrance, reset boss health, and start phase 2
     // if they succeed to hit the QTE's reset the players position to the entrance and set boss health to 1 (1 shottable)
@@ -57,7 +63,8 @@ public class RT_BossAI : MonoBehaviour
 
     // Play boss song
     // Do Guitar Solo Lane Attack
-    // If you hit the boss 1 time you can trigger the final QTE's which is 11 in total
+    // If boss health reaches 0 stagger the boss and allow the boss to be hit to trigger a QTE event
+    // 11 QTE's happen and the player needs to hit them to beat the boss
     // if they fail to hit the QTE's reset the players position to the entrance, reset boss health to 1 and restart phase 3
     // if they succeed to hit the QTE's play the boss's death cutscene
 
