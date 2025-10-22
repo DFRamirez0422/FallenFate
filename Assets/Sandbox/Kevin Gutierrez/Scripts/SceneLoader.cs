2f128f09b765af47d0b2ce421a1da4c0c7c553b5
Kevin Gutierrez <kevin.gutierrez012@mymdc.net>
2025-10-22 18:11:54 -0400

    successfully transfered the important UI elements for menu scenes

---
+++ Assets/Sandbox/Kevin Gutierrez/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+public class SceneLoader : MonoBehaviour
+{
+public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu1");
+    }
+}
