4a7cc29198e49b2c4b87bea2c0d1cf2853f852ee
JoseEscobedo02 <jose.escobedo002@mymdc.net>
2026-01-14 19:34:57 -0500

    Initial commit.

---
+++ Assets/Scripts/CameraBoundFinder.cs
@@ -0,0 +1,30 @@
+using Unity.Burst.Intrinsics;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraBoundFinder : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        CinemachineConfiner2D confiner = GetComponent<CinemachineConfiner2D>();
+        GameObject camera_bounds = GameObject.FindWithTag("CameraBounds");
+        Collider2D collider = camera_bounds.GetComponent<PolygonCollider2D>();
+
+        if (!collider)
+        {
+            collider = camera_bounds.GetComponent<BoxCollider2D>();
+        }
+
+        confiner.BoundingShape2D = collider;
+    }
+}
