017dc4ffe4a2a4b3d0c70f8f757ce6c026dd4446
NathanFWhite <Nathan.White003@mymdc.net>
2026-01-20 09:11:21 -0500

    Warden start
    Started working on the warden

---
+++ Assets/Scripts/Warden Scripts/WardenJumpScare.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WardenJumpScare : MonoBehaviour
+{
+    public Image jumpscareImage;
+    public AudioClip jumpscareClip1;
+    public AudioSource jumpscareSource;
+
+    private SplayerHealth health;
+    private WardenMovement movement;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health = collision.gameObject.GetComponent<SplayerHealth>();
+        Debug.Log("Collided with: " + collision.gameObject.name);
+
+        if (health.currentHealth > 0)
+        {
+            health.ChangeHealth(-2);
+            movement.stunned = true;
+        }
+        else if (health.currentHealth <= 0)
+        {
+            jumpscareImage.enabled = true; Debug.Log("Enabled Image");
+
+            if (jumpscareClip1 != null)
+            {
+                jumpscareSource.PlayOneShot(jumpscareClip1);
+            }
+            StartCoroutine(CloseJumpscare());
+        }
+
+        
+    }
+
+    private IEnumerator CloseJumpscare()
+    {
+        yield return new WaitForSeconds(2);
+        jumpscareImage.enabled = false;
+    }
+}
--- Assets/Scripts/Warden Scripts/WardenMovement.cs
+++ Assets/Scripts/Warden Scripts/WardenMovement.cs
@@ -1,12 +1,15 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Enemy_Movement : MonoBehaviour
+public class WardenMovement : MonoBehaviour
 {
     public float speed;
     private bool isChasing;
 
     private Rigidbody2D rb;
     private Transform player;
+
+    public bool stunned;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,23 +20,34 @@ public class Enemy_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isChasing == true) { 
-        Vector2 direction = (player.position - transform.position).normalized;
-        rb.velocity = direction * speed;
+        if (isChasing == true)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.velocity = direction * speed;
         }
+
+        if (stunned == true)
+        {
+            Invoke(nameof(Unstun), 2);
+        }
+    }
+
+    private void Unstun()
+    {
+        stunned = false;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (player == null)
+            if (player == null && !stunned)
             {
                 player = collision.transform;
             }
             isChasing = true;
         }
-        
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
