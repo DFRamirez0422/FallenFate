ad01d99477d9770101c5c6edd5f079a92e8012db
JoseEscobedo02 <jose.escobedo002@mymdc.net>
2026-02-06 22:48:50 -0500

    Added a player find feature in the health bar.
    It does add some coupling but considering for the most part that the health bar is connected to the player health, it is reasonable that it can be automaically set up.

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -6,6 +6,12 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private TMP_Text m_HealthBarText;
     [SerializeField] private PlayerHealth m_Health;
 
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        m_Health = player.GetComponent<PlayerHealth>();
+    }
+
     void Update()
     {
         m_HealthBarText.text = $"HP: {m_Health.CurrentHealth}/{m_Health.MaxHealth}";
