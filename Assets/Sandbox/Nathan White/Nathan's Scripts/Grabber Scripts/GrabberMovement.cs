70a7d8c40bf93785425054ced364cd45e180a6e4
NathanFWhite <Nathan.White003@mymdc.net>
2026-02-12 14:01:55 -0500

    fixed other Grabbers moving after player gets grabbed

--- Assets/Sandbox/Nathan White/Nathan's Scripts/Grabber Scripts/GrabberMovement.cs
+++ Assets/Sandbox/Nathan White/Nathan's Scripts/Grabber Scripts/GrabberMovement.cs
@@ -1,14 +1,18 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GrabberMovement : MonoBehaviour
 {
     public float speed;
     public bool isChasing;
-
+    [SerializeField]
+    public static bool SomeoneGrabbedPlayer = false;
 
     public Rigidbody2D rb;
     private Transform player;
     private Animator animator;
+    private ButtonMash ButtonMash;
+    private Vector2 direction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,17 +20,28 @@ public class GrabberMovement : MonoBehaviour
         animator = GetComponentInParent<Animator>();
         rb = GetComponentInParent<Rigidbody2D>();
         animator.SetBool("Chasing", false);
+        ButtonMash = GetComponentInParent<ButtonMash>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isChasing == true && !animator.GetBool("Attacking"))
+        if (isChasing == true && !animator.GetBool("Attacking") && SomeoneGrabbedPlayer == false)
         {
-            Vector2 direction = (player.position - transform.position).normalized;
+            direction = (player.position - transform.position).normalized;
             rb.linearVelocity = direction * speed;
         }
 
+        if (ButtonMash.started == true)
+        {
+            SomeoneGrabbedPlayer = true;
+        }
+
+        if (SomeoneGrabbedPlayer == true)
+        {
+            direction = transform.position;
+            rb.linearVelocity = Vector2.zero;
+        }
 
         if (animator.GetBool("Attacking"))
         {
@@ -38,7 +53,6 @@ public class GrabberMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-           
             if (player == null)
             {
                 player = collision.transform;
@@ -47,7 +61,10 @@ public class GrabberMovement : MonoBehaviour
             animator.SetBool("Chasing", true);
             isChasing = true;
         }
-
+        if (collision.gameObject.tag == "Player" && SomeoneGrabbedPlayer == true)
+        {
+            isChasing = false;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -65,5 +82,10 @@ public class GrabberMovement : MonoBehaviour
         isChasing = false;
         rb.linearVelocity = Vector2.zero;
         Debug.Log("Stop");
+    }
+
+    public void StoppedGrabbing()
+    {
+        SomeoneGrabbedPlayer = false;
     }
 }
