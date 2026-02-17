f99ad96563ce13391957d5bf9e7e6fb3f52d94eb
NathanFWhite <Nathan.White003@mymdc.net>
2026-02-17 10:35:22 -0500

    Finished the Prototype of Angry Echo

--- Assets/Sandbox/Nathan White/Nathan's Scripts/Echo Scripts/Nathan's Test Red Echo/AngryEchoAi.cs
+++ Assets/Sandbox/Nathan White/Nathan's Scripts/Echo Scripts/Nathan's Test Red Echo/AngryEchoAi.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class AngryEchoAi : MonoBehaviour
 {
+    public GameObject Hitbox;
+    private Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -13,4 +16,22 @@ public class AngryEchoAi : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetBool("IsAttacking", true);
+    }
+
+    private void TurnOnHitbox()
+    {
+        Hitbox.SetActive(true);
+        Invoke(nameof(TurnOffHitbox), 1);
+    }
+
+    private void TurnOffHitbox()
+    {
+        Hitbox.SetActive(false);
+        animator.SetBool("IsAttacking", false);
+    }
+
 }
