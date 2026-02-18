db8594b22c851aeda42f077227c6671479322f73
Michael Hernandez <mikegtr_305@hotmail.com>
2026-02-18 12:01:13 -0500

    PlayerScript Update/Fix
    Added a new function, TakeDamage, to grab enemy transform and properly use it for hit reactions on player. ChangeHealth is back to original form and we can now call TakeDamage within it if needed to grab enemy transform.

--- Assets/Sandbox/Nathan White/Nathan's Scripts/Warden Scripts/WardenJumpScare.cs
+++ Assets/Sandbox/Nathan White/Nathan's Scripts/Warden Scripts/WardenJumpScare.cs
@@ -35,7 +35,7 @@ public class WardenJumpScare : MonoBehaviour
         {
             if (health.CurrentHealth > 0)
             {
-                health.ChangeHealth(-2,null);
+                health.ChangeHealth(-2);
                 movement.stunned = true;
                 text.text = "Damaged";
                 TextCanvas.SetActive(true);
