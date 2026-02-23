ecc6a040375c94c1a751e1a7052811717432b213
Altodor <rodneytorres.dev@gmail.com>
2026-02-23 11:47:06 -0500

    Revert "QuestUI"

--- Assets/Sandbox/Angel_Rodriguez/Quest/LocationSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LocationSO", menuName = "ScriptableObjects/LocationSO", order = 2)]
-public class LocationSO : ScriptableObject
-{
-    public string locationID;
-    public string locationName;
-}
