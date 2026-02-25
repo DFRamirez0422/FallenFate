0c475c5f3603b52d388be7c44010455c274d3fd7
NathanFWhite <Nathan.White003@mymdc.net>
2026-02-25 08:35:16 -0500

    Added direction toggles for both echo's

--- Assets/Sandbox/Nathan White/Nathan's Scripts/Echo Scripts/Echo Ai.cs
+++ Assets/Sandbox/Nathan White/Nathan's Scripts/Echo Scripts/Echo Ai.cs
@@ -5,6 +5,8 @@ public class EchoAi : MonoBehaviour
     private Transform player;
     private ObjectFader objectFader;
 
+    public bool FaceAway;
+
     private void Start()
     {
         objectFader = GetComponent<ObjectFader>();
@@ -17,6 +19,32 @@ public class EchoAi : MonoBehaviour
         {
             SelfDestroy();
         }
+
+        if (FaceAway)
+        {
+            Transform player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            // Calculate the direction from the trigger (this.transform.position) 
+            // to the other object (other.transform.position)
+            Vector2 directionToTarget = player.transform.position - this.transform.position;
+
+            // Normalize the vector to get only the direction with a magnitude (length) of 1
+            Vector2 normalizedDirection = directionToTarget.normalized;
+
+            // You can now use normalizedDirection for various purposes
+            //Debug.Log("Direction to " + collision.gameObject.name + ": " + normalizedDirection);
+
+            if (normalizedDirection.x < -0.1f)
+            {
+                // Resets to face forward when moving right
+                transform.eulerAngles = new Vector2(0, 0);
+            }
+            else if (normalizedDirection.x > 0.1f)
+            {
+                // Sets the rotation exactly to 0, 180, 0
+                transform.eulerAngles = new Vector2(0, 180);
+            }
+        }
+
     }
 
     public void SelfDestroy()
