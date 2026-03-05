a9900f754a7c5e62b1ac7b3080976bb8de483f04
luisespinoza006 <luis.espinoza006@mymdc.net>
2026-03-05 11:58:51 -0500

    Revert "Merge branch 'main' into CheatMenu_03_04_2026"
    This reverts commit 6ab57defcfb7f945db7ec7dd92b55efd2bbfd515, reversing
    changes made to ebb7a91d7b7d082f5f17a42c075578e3a55597aa.

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -1,48 +1,19 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    [Header("Health Settings")]
-    public int maxHealth = 4;
-    private int currentHealth;
-
-    [Header("Heart Sprites")]
-    public Sprite fullHeart;
-    public Sprite halfHeart;
-    public Sprite emptyHeart;
-
-    [Header("Heart Images")]
-    public Image[] hearts;
-
-    private PlayerHealth m_Health;
+    [SerializeField] private TMP_Text m_HealthBarText;
+    [SerializeField] private PlayerHealth m_Health;
 
     void Start()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         m_Health = player.GetComponent<PlayerHealth>();
-
-        UpdateHearts();
     }
 
     void Update()
     {
-        currentHealth = m_Health.CurrentHealth;
-        UpdateHearts();
-    }
-
-    void UpdateHearts()
-    {
-        for (int i = 0; i < hearts.Length; i++)
-        {
-            if (i < currentHealth)
-            {
-                hearts[i].sprite = fullHeart;
-            }
-            else
-            {
-                hearts[i].sprite = emptyHeart;
-            }
-        }
+        m_HealthBarText.text = $"HP: {m_Health.CurrentHealth}/{m_Health.MaxHealth}";
     }
 }
