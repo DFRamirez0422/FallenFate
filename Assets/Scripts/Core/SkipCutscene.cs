b7d2b084c4ed19160a4b1ea6644a2e474b5aa798
peachyteaz <a.baluja0729@gmail.com>
2026-03-17 11:52:43 -0400

    fixes skiped cutscene buttons

--- Assets/Scripts/Core/SkipCutscene.cs
+++ Assets/Scripts/Core/SkipCutscene.cs
@@ -5,22 +5,26 @@ using UnityEngine.SceneManagement;
 public class SkipCutscene : MonoBehaviour
 {
     public string sceneToLoad;
-    [SerializeField] private UnityEvent m_OnSkipCutsceneEvent;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Start()
     {
-        if (Input.GetKeyDown("Interact"))
-        {
-            m_OnSkipCutsceneEvent?.Invoke();
-            SceneManager.LoadScene(sceneToLoad);
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            LoadScene(sceneToLoad);
+        }
+
+    }
+
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
--- Assets/Scripts/Core/sceneChangeNoPlayerObj.cs
+++ Assets/Scripts/Core/sceneChangeNoPlayerObj.cs
@@ -8,24 +8,24 @@ public class SceneChangeNoPlayerObj : MonoBehaviour
     [SerializeField] private string m_SceneName;
 
     [Tooltip("Animator component for the fade screen effect.")]
-    [SerializeField] private Animator m_FadeScreenAnimator;
+    [SerializeField] public Animator m_FadeScreenAnimator;
 
     [Tooltip("Amount of time to wait before loading the next scene.")]
     [SerializeField] private float m_FadeScreenTime = 0.5f;
 
-    public void TransitionSceneCutscene()
-    {
-        if (m_FadeScreenAnimator)
-        {
-            m_FadeScreenAnimator.Play("FadeOut");
-        }
+    //public void TransitionSceneCutscene()
+    //{
+    //    if (m_FadeScreenAnimator)
+    //    {
+    //        m_FadeScreenAnimator.Play("FadeOut");
+    //    }
 
-        StartCoroutine(DelayFade());
-    }
+    //    StartCoroutine(DelayFade());
+    //}
 
-    private IEnumerator DelayFade()
-    {
-        yield return new WaitForSeconds(m_FadeScreenTime);
-        SceneManager.LoadScene(m_SceneName);
-    }
+    //private IEnumerator DelayFade()
+    //{
+    //    yield return new WaitForSeconds(m_FadeScreenTime);
+    //    SceneManager.LoadScene(m_SceneName);
+    //}
 }
