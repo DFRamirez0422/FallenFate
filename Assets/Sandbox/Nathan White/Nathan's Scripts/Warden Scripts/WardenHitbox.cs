a59273f81ff3c9307bf9e13fe987cca5c6e56529
NathanFWhite <Nathan.White003@mymdc.net>
2026-03-17 08:31:24 -0400

    Fixed the wardens attacks

---
+++ Assets/Sandbox/Nathan White/Nathan's Scripts/Warden Scripts/WardenHitbox.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class WardenHitbox : MonoBehaviour
+{
+    private PlayerHealth health;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.gameObject.tag == "Hitboxs")
+        {
+            Debug.Log("Should hit you");
+            health.TakeDamage(2, transform);
+        }
+    }
+}
