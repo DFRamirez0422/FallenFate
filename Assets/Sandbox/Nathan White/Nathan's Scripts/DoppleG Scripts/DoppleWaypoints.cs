583140ea6c49f513fe29c20904527377651d52f3
NathanFWhite <Nathan.White003@mymdc.net>
2026-03-31 08:06:28 -0400

    Added a wall that only enemies can collide with

--- Assets/Sandbox/Nathan White/Nathan's Scripts/DoppleG Scripts/DoppleWaypoints.cs
+++ Assets/Sandbox/Nathan White/Nathan's Scripts/DoppleG Scripts/DoppleWaypoints.cs
@@ -29,6 +29,15 @@ public class Waypoints : MonoBehaviour
         {
             IsMoving = false;
         }
+
+        if (IsMoving == false)
+        {
+            animator.SetBool("Idle", true);
+        }
+        else if (IsMoving == true)
+        {
+            animator.SetBool("Idle", false);
+        }
     }
 
     public void Move()
