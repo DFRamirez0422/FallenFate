158abe17a78ff709e0bf8a2bca9a4b4f370a7c6e
Epicdavid12 <dglazier613@gmail.com>
2026-04-05 21:23:55 -0400

    Revert "polished UI in act 2 and the Garden"
    This reverts commit f5db27f12a9831e79fc658d030aa29d59592824d.

--- Assets/Sandbox/Kevin G/Scripts/AspectRatioLock.cs
+++ Assets/Sandbox/Kevin G/Scripts/AspectRatioLock.cs
@@ -2,26 +2,16 @@ using UnityEngine;
 
 public class AspectRatioLock : MonoBehaviour
 {
-    [Header("Camera Settings")]
     public float targetAspect = 1920f / 1080f;
 
-    [Header("UI Settings")]
-    public RectTransform uiRoot; // Parent of all UI elements
-
     void Start()
-    {
-        LockCameraAspect();
-        ClampUI();
-    }
-
-    void LockCameraAspect()
     {
         float windowAspect = (float)Screen.width / Screen.height;
         float scaleHeight = windowAspect / targetAspect;
 
         Camera cam = GetComponent<Camera>();
 
-        if (scaleHeight < 1.0f) // Black bars top/bottom
+        if (scaleHeight < 1.0f)
         {
             Rect rect = cam.rect;
             rect.width = 1.0f;
@@ -30,7 +20,7 @@ public class AspectRatioLock : MonoBehaviour
             rect.y = (1.0f - scaleHeight) / 2.0f;
             cam.rect = rect;
         }
-        else // Black bars left/right
+        else
         {
             float scaleWidth = 1.0f / scaleHeight;
             Rect rect = cam.rect;
@@ -40,21 +30,5 @@ public class AspectRatioLock : MonoBehaviour
             rect.y = 0;
             cam.rect = rect;
         }
-    }
-
-    void ClampUI()
-    {
-        if (uiRoot == null) return;
-
-        // Reset position to center
-        uiRoot.localPosition = Vector3.zero;
-
-        float windowAspect = (float)Screen.width / Screen.height;
-        float scaleHeight = windowAspect / targetAspect;
-
-        float scaleFactor = (scaleHeight < 1f) ? scaleHeight : 1f / scaleHeight;
-
-        // Apply scale without moving the UI out of center
-        uiRoot.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
     }
 }
