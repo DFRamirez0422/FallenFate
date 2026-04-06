60a38e7331982aa701a99caaa566c6e7ffb5215c
Epicdavid12 <dglazier613@gmail.com>
2026-04-05 23:17:08 -0400

    Merge branch 'main' into Angel-Fixes-4/4/26
    fixed angel's quest trigger for front gate
    fixed door open no key library.
    fixed library door
    adjusted colliders in act 2

--- Assets/Sandbox/Angel_Rodriguez/Resources/Scripts/OpenDoor_NoKey.cs
+++ Assets/Sandbox/Angel_Rodriguez/Resources/Scripts/OpenDoor_NoKey.cs
@@ -42,7 +42,7 @@ public class OpenDoor_NoKey : CollidableObject
     void OnCollisionEnter2D(Collision2D collision)
     {
         Collider2D hitCollider = collision.collider;
-        if (hitCollider.CompareTag("Hitboxs"))
+        if (hitCollider.CompareTag("Hitboxs") || hitCollider.CompareTag("Player"))
         {
                 _SpawnedPrompt = Instantiate(OpenDoorPrompt);
                 _SpawnedPrompt.GetComponentsInChildren<Text>()[0].text = "Open Door";
@@ -57,7 +57,7 @@ public class OpenDoor_NoKey : CollidableObject
     {
 
         Collider2D hitCollider = collision.collider;
-        if (hitCollider.CompareTag("Hitboxs"))
+        if (hitCollider.CompareTag("Hitboxs") || hitCollider.CompareTag("Player"))
         {
             _SpawnedPrompt.SetActive(false);
             _SpawnedPrompt.GetComponentsInChildren<Text>()[0].text = "";
